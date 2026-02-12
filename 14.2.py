03b92367352803b4ad04defd119f0edb894e276d
tasoszaf <168640996+tasoszaf@users.noreply.github.com>
2026-02-12 10:58:02 +0200

    Update 14.2.py

--- 14.2.py
+++ 14.2.py
@@ -1,17 +1,33 @@
 import streamlit as st
 
+# Ρυθμίσεις σελίδας
 st.set_page_config(page_title="Be My Valentine 💖", page_icon="❤️", layout="centered")
 
-st.title("Will you be my Valentine? 💌")
-st.write("Please choose wisely...")
+# Τίτλος και οδηγίες
+st.markdown("<h1 style='text-align: center; color: pink;'>Will you be my Valentine? 💌</h1>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center;'>Choose wisely...</p>", unsafe_allow_html=True)
 
+# Δημιουργία δύο στηλών για τα κουμπιά
 col1, col2 = st.columns(2)
 
 with col1:
-    if st.button("Yes"):
-        st.success("Yay! 💖 You made me so happy!")
+    if st.button("Yes 💖"):
+        st.balloons()  # Confetti effect
+        st.success("Yay! You made me so happy! 💕")
+        st.markdown("<h2 style='color: red;'>I ❤️ you!</h2>", unsafe_allow_html=True)
 
 with col2:
-    # Κουμπί "Όχι" απενεργοποιημένο
-    st.button("No", disabled=True)
-    st.write("you are officialy my date")  
+    st.button("No ❌", disabled=True)  # Απενεργοποιημένο κουμπί
+    st.write("YOU ARE OFFICIALLY MY DATE!!!")  # Προαιρετικό σχόλιο
+
+# Προσθέτουμε λίγο στυλ για πιο cute εμφάνιση
+st.markdown(
+    """
+    <style>
+    body {
+        background-color: #ffe6f0;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
